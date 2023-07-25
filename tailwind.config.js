72148435f9f8ec4deb1cc0a79b4ee073c8bc8b59
Sujeong Artlab <sujeong@artlab.ai>
2023-07-25 22:05:10 +0900

    fix: tailwind config의 html 경로 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./src/**/*.{html}",  "./src/*.{html}", './src/blog-section-three-column.html'], 
-  // "./src/**/*.{js, ts, html}", 
-  // ,'./src/blog-section-three-column.html', 
+  content: ["./index.html","./src/**/*.html"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
