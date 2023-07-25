c4375b925a329fe0739494224bdaea3e819f8c93
Sujeong Artlab <sujeong@artlab.ai>
2023-07-25 21:51:03 +0900

    blog section: three column

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html","./src/**/*.{html}",  "./src/*.{html}", './src/blog-section-three-column.html'], 
+  // "./src/**/*.{js, ts, html}", 
+  // ,'./src/blog-section-three-column.html', 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
